9859ab57ebb8844cd61c5856f2681b1948c5a56d
Mees Hollebeek <Mees.hollebeek@gmail.com>
2022-06-25 15:14:29 +0200

    cartoon shader

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -29,12 +29,16 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        if (!glitchOnRotate.isPaused)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+            Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+            controller.Move(move * speed * Time.deltaTime);
+
+        }
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
--- Assets/scripts/glitchOnRotate.cs
+++ Assets/scripts/glitchOnRotate.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class glitchOnRotate : MonoBehaviour
 {
     public Transform playerBody;
+    public GlitchEffect script;
+    public static bool isPaused;
 
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        
     }
 
     // Update is called once per frame
@@ -19,9 +21,36 @@ public class glitchOnRotate : MonoBehaviour
     {
        if(playerBody.transform.rotation.y > 0.67 || playerBody.transform.rotation.y < -0.67)
         {
+            script.intensity = 1;
+            isPaused = true;
+            
+
+
+        }
+       else
+        {
+            script.intensity = 0;
+            isPaused = false;
             
         }
 
-       //Debug.Log(playerBody.transform.rotation.y);
+        if (playerBody.transform.rotation.y > 0.75 || playerBody.transform.rotation.y < -0.75)
+        {
+            script.flipIntensity = 1;
+        }
+        else
+        {
+            script.flipIntensity = 0;
+        }
+
+        if (playerBody.transform.rotation.y > 0.85 || playerBody.transform.rotation.y < -0.85)
+        {
+            script.colorIntensity = 1;
+        }
+        else
+        {
+            script.colorIntensity = 0;
+        }
+
     }
 }
